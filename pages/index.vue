f790d5dfd27308b18c4b87347e83d534ea5c7fc9
hikarut <ht.hikaru.takahashi@gmail.com>
2019-12-07 23:51:15 +0900

    nuxtのテンプレ

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    aaa
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  color: $black;
+}
+</style>

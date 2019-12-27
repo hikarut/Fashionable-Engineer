68492883f1b9b0c0654b54bd4626d63913164a54
hikarut <ht.hikaru.takahashi@gmail.com>
2019-12-27 11:17:21 +0900

    着用アイテム追加

--- pages/404.vue
+++ pages/404.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="hero is-medium">
     <div class="hero-body">
-      <div class="container">
+      <div class="container center-text">
         <h1>
           エラーです。
         </h1>
@@ -22,7 +22,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container {
-  text-align: center;
-}
 </style>
--- pages/coordinate/_id.vue
+++ pages/coordinate/_id.vue
@@ -12,6 +12,21 @@
         <p class="description" v-html="item.description" />
       </div>
     </div>
+
+    <h3 class="subtitle is-5 center-text">着用アイテム</h3>
+    <div class="columns is-mobile is-multiline wear-items">
+      <div v-for="(name, j) in wear"
+           v-if="item[name] !== null"
+           :key="j"
+           class="column is-one-third">
+        <figure class="wear-image">
+          <img v-lazy="imageUrl(item[name].img.url)">
+        </figure>
+        <p class="wear-brand center-text">{{ item[name].brand }}</p>
+        <p class="wear-name center-text">{{ item[name].name }}</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -22,6 +37,9 @@ import { resizeImageUrl } from '~/lib/url'
 
 export default {
   components: {},
+  data: () => ({
+    wear: process.env.CONSTANT.ITEM_LIST
+  }),
   methods: {
     convert(str) {
       return dateString(str)
@@ -52,6 +70,10 @@ export default {
     width: 400px;
     margin: auto;
   }
+  .wear-image {
+    width: 150px;
+    margin: auto;
+  }
 }
 // 上書き
 .columns {
@@ -62,5 +84,18 @@ export default {
 .description {
   margin-top: 20px;
   margin-bottom: 20px;
+}
+.wear-image {
+  width: 100px;
+  margin: auto;
+}
+.wear-brand {
+  font-size: 10px;
+}
+.wear-name {
+  font-size: 8px;
+}
+.wear-items {
+  margin-bottom: 30px;
 }
 </style>
